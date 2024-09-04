cf0990da82b8b2ecbd4441cde64fa7204059c98a
arpit mishra <arpit.mishra.out@gmail.com>
2024-09-04 07:11:07 +0530

    javascript

--- js_concepts/01.js
+++ js_concepts/01.js
@@ -26,9 +26,9 @@ function getName(){
 // console.log(getName);
 
 var x = 1;
-a();
-b();
-console.log(x);
+// a();
+// b();
+// console.log(x);
 function a(){
     var x = 10;
     console.log(x);
@@ -38,3 +38,54 @@ function b(){
     var x = 100;
     console.log(x);
 }
+
+
+// undefined vs not defined  
+
+function a(){
+    console.log(b);
+}
+
+a();
+var b = 10;
+
+{
+    // var a = 10;
+    // console.log(a);
+}
+// if(true) console.log(true);
+
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a); // 10
+    // console.log(b); // 20
+    // console.log(c); // 30
+
+}
+console.log(a); // 10
+    // console.log(b); // refrence error
+    // console.log(c); // refrence error
+
+
+    function x(){
+        var a = 7;
+        function y(){
+            console.log(a);
+        }
+        y();
+    }
+    // x();
+
+
+function p(){
+    var a = 35;
+    function q(){
+        console.log(a);
+    }
+    return q;
+}
+
+var z = p();
+console.log(z);
